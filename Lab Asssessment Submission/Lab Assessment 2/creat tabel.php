014236c4faeac90550a35bcfae9a30015281d3fc
mofakkharulislam <mofakkharul.islam@northsouth.edu>
2020-07-18 10:53:04 +0600

    Lab Assessment 2

--- Lab Asssessment Submission/Lab Assessment 2/creat tabel.php
+++ Lab Asssessment Submission/Lab Assessment 2/creat tabel.php
@@ -1,29 +1,27 @@
 <?php
 
-$link =mysqli_connect('localhost','root','','VS_CODE_TEST');
+$link = mysqli_connect('localhost','root','','VS_CODE_TEST');
 
 if($link == false)
 {
-    die ("Error:Could Not connect.".mysqli_connect_error());
+    die("Error: Could not connect. " .mysqli_connect_error());
 }
 
-$sql ="CREATE TABLE test_table(
-    id NOT NULL PRIMARY KEY AUTO_INCREMENT,
+
+$sql = "CREATE TABLE test_table(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(50) NOT NULL UNIQUE,
-    password  VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-    
     )";
 
-if(mysqli_query($link,$sql)){
+if(mysqli_query($link, $sql)){
     echo "Table Created";
-
 }
-else
-{
-    echo "Error: Could not able to create Table." .mysqli_error($link);
+else{
+    echo "Error: Could not create Table" .mysqli_error($link);
 }
 
-mysqli_close($link)
+mysqli_close($link);
 
 ?>
